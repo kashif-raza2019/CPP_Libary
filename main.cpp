2ba2dd649c93ef15197c9c13290f27906c48cafa
Kashif Raza <kashifraza08012001@gmail.com>
2022-07-13 13:52:35 +0530

    Main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 int main(){
-    BigInt num("12323224244234422364654565543224");
-    cout<<num;
+    BigInt num1("12323224244234422364654565543224");
+    BigInt num2 = 1234567891011121314151617181920;
+    BigInt sum = num1 + num2;
+    BigInt multiply = num1*num2;
+    BigInt squareRoot = sqrt(multiply);
+    cout<<"Num 1: "<<num1<<" Num2: "<<num2<<endl;
+    cout<<"Sum of Num1 and Num2 : "<<sum<<endl;
+    cout<<"Multiplication of Both Numbers : "<<multiply<<endl;
+    cout<<"Square Root of multiply: "<<squareRoot<<endl;
     return 0;
 }
